ca872d6a11c1c906caec53ac2155972b7d6c7190
koyama <kmiccho@gmail.com>
2022-08-15 23:10:32 +0900

    Update: articleのgraphqlの項目を追加する

--- web/backend/src/article/presentation/dto/article.dto.ts
+++ web/backend/src/article/presentation/dto/article.dto.ts
@@ -1,16 +1,23 @@
-import { Field, ObjectType } from '@nestjs/graphql'
+import { Field, ID, ObjectType } from '@nestjs/graphql'
 
 @ObjectType('Article', {
   isAbstract: true,
 })
 export class ArticleDto {
-  private constructor(title: string, content: string) {
+  private constructor(id: number, title: string, content: string, published: boolean) {
+    this.id = id
     this.title = title
     this.content = content
+    this.published = published
   }
-  static fromEntity(entity: { title: string; content: string }) {
-    return new ArticleDto(entity.title, entity.content)
+  static fromEntity(entity: { id: number; title: string; content: string; publisehd: boolean }) {
+    return new ArticleDto(entity.id, entity.title, entity.content, entity.publisehd)
   }
+
+  @Field(() => ID, {
+    nullable: false,
+  })
+  id!: number
 
   @Field(() => String, {
     nullable: false,
@@ -21,4 +28,10 @@ export class ArticleDto {
     nullable: false,
   })
   content!: string
+
+  @Field(() => Boolean, {
+    nullable: false,
+  })
+  published!: boolean
+
 }
