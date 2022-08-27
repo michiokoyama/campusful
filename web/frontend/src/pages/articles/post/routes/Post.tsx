4ac01876b242da8ce413a6d90cafe91a3417a937
koyama <kmiccho@gmail.com>
2022-08-27 16:27:35 +0900

    Fix: ボタンが動作するように修正する

--- web/frontend/src/pages/articles/post/routes/Post.tsx
+++ web/frontend/src/pages/articles/post/routes/Post.tsx
@@ -7,6 +7,25 @@ import {
 } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 import { Header } from '../../../../components/common/Header';
+import {
+  Box,
+  Button,
+  Flex,
+} from '@chakra-ui/react';
+
+const InlineButtons = [
+  {type: "BOLD", text: "bold"},
+  {type: "ITALIC", text: "italic"},
+  {type: "STRIKETHROUGH", text: "strikthrough"},
+]
+
+const BlockStyleButtons = [
+  {type: "header-one", text: "H1"},
+  {type: "header-two", text: "H2"},
+  {type: "header-three", text: "H3"},
+  {type: "unstyled", text: "Normal"},
+]
+
 
 const TextEditor = () => {
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty())
@@ -20,7 +39,7 @@ const TextEditor = () => {
     return "not-handled";
   };
 
-  const handleTogggleClick = (e: React.MouseEvent, inlineStyle: string) => {
+  const handleToggleClick = (e: React.MouseEvent, inlineStyle: string) => {
     e.preventDefault();
     setEditorState(RichUtils.toggleInlineStyle(editorState, inlineStyle));
   };
@@ -32,30 +51,37 @@ const TextEditor = () => {
 
   return (
     <>
-      <button
+      <Button
         disabled={editorState.getUndoStack().size <= 0}
         onMouseDown={() => setEditorState(EditorState.undo(editorState))}>
         undo
-      </button>
-      <button
+      </Button>
+      <Button
         disabled={editorState.getRedoStack().size <= 0}
         onMouseDown={() => setEditorState(EditorState.redo(editorState))}>
         redo
-      </button>
-      <button className='RichEditor-styleButton' onMouseDown={(e) => handleBlockClick(e, "header-one")}>H1</button>
-      <button onMouseDown={(e) => handleBlockClick(e, "header-two")}>H2</button>
-      <button onMouseDown={(e) => handleBlockClick(e, "header-three")}>H3</button>
-      <button onMouseDown={(e) => handleBlockClick(e, "unstyled")}>Normal</button>
-      <button onMouseDown={(e) => handleTogggleClick(e, "BOLD")}>bold</button>
-      <button onMouseDown={(e) => handleTogggleClick(e, "ITALIC")}>italic</button>
-      <button onMouseDown={(e) => handleTogggleClick(e, "STRIKETHROUGH")}>strikthrough</button>
-      <button onMouseDown={(e) => handleBlockClick(e, "ordered-list-item")}>Ordered List</button>
-      <button onMouseDown={(e) => handleBlockClick(e, "unordered-list-item")}>Unordered List</button>
-      <Editor editorState={editorState} onChange={setEditorState} handleKeyCommand={handleKeyCommand} />
+      </Button>
+      <Flex>
+      {BlockStyleButtons.map(button => {
+        return <Button onMouseDown={(e) => handleBlockClick(e, button.type)}>{button.text}</Button>
+      })}
+      </Flex>
+      <Flex>
+      {InlineButtons.map(button => {
+        return <Button onMouseDown={(e) => handleToggleClick(e, button.type)}>{button.text}</Button>
+      })}
+      <Button onMouseDown={(e) => handleBlockClick(e, "ordered-list-item")}>Ordered List</Button>
+      <Button onMouseDown={(e) => handleBlockClick(e, "unordered-list-item")}>Unordered List</Button>
+      </Flex>
+      <Box
+        borderWidth='2px'
+        height='70vh'
+      >
+        <Editor editorState={editorState} onChange={setEditorState} handleKeyCommand={handleKeyCommand} />
+      </Box>
     </>
   );
 }
-
 
 export const Post = () => {
   return (
