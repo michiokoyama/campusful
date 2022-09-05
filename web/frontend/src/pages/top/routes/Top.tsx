a9701f220ec789fb2ee431e9ee90b3cf8a2d03db
koyama <kmiccho@gmail.com>
2022-09-05 18:45:13 +0900

    Update: カテゴリ、大学名、投稿日などを表示するように修正する。

--- web/frontend/src/pages/top/routes/Top.tsx
+++ web/frontend/src/pages/top/routes/Top.tsx
@@ -1,7 +1,10 @@
-import { Box, ChakraStyledOptions } from '@chakra-ui/react'
+import { Badge, Box, ChakraStyledOptions } from '@chakra-ui/react'
 import { HeaderWithSidebar } from '../../../components/common/Header';
 import { SearchBox } from '../../../components/common/SearchBox';
 import { useArticleLists } from '../hooks/useArticleList'
+import {
+  StarIcon
+} from '@chakra-ui/icons';
 
 export const Top = () => {
   return (
@@ -21,26 +24,63 @@ const MainContents = () => {
   </>)
 }
 
-const props = [
-  {
-    name: '氏名1',
-    // universityName: '大学名1',
-    title: 'タイトル1',
-    content: '本文1',
-    // reviewCount: 8,
-    // rating: 2,
-  },
-  {
-    name: '氏名2',
-    // universityName: '大学名2',
-    title: 'Toooooooooooooooooooooooooooooooooooooooooo long texts. Modern home in city center in the heart of historic Los Angeles',
-    content: 'Toooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo long text',
-    // reviewCount: 34,
-    // rating: 4,
-  },
-
-]
 const ArticleLists = (props: ChakraStyledOptions) => {
+  console.log(props)
+  return (
+    <Box maxW='xg' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+      <Box p='6'>
+        <Box display='flex' alignItems='baseline'>
+          <Badge borderRadius='full' px='2' colorScheme='teal'>
+            {props.category.name}
+          </Badge>
+          <Box
+            color='gray.500'
+            fontWeight='semibold'
+            letterSpacing='wide'
+            fontSize='xs'
+            textTransform='uppercase'
+            ml='2'
+          >
+            {props.author.university.name} 
+          </Box>
+        </Box>
+
+        <Box
+          mt='1'
+          fontWeight='semibold'
+          as='h4'
+          lineHeight='tight'
+          noOfLines={1}
+        >
+          {props.title}
+        </Box>
+
+        <Box
+          noOfLines={3}
+        >
+          {props.content}
+        </Box>
+
+        <Box display='flex' mt='2' alignItems='center'>
+          <Box as='span' ml='2' color='gray.600' fontSize='sm'>
+            投稿日: 2022/9/5
+          </Box>
+
+          {Array(5)
+            .fill('')
+            .map((_, i) => (
+              <StarIcon
+                key={i}
+                color={i < 0 ? 'teal.500' : 'gray.300'}
+              />
+            ))}
+          <Box as='span' ml='2' color='gray.600' fontSize='sm'>
+            13 reviews
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  )
 
   return (
     <Box maxW='xg' borderWidth='1px' borderRadius='lg' overflow='hidden'>
@@ -58,14 +98,21 @@ const ArticleLists = (props: ChakraStyledOptions) => {
             {props.name}
           </Box>
         </Box>
-        <Box
-          mt='1'
-          fontWeight='semibold'
-          as='h4'
-          lineHeight='tight'
-          noOfLines={1}
-        >
-          {props.title}
+        <Box>
+          <Box
+            mt='1'
+            fontWeight='semibold'
+            as='h4'
+            lineHeight='tight'
+            noOfLines={1}
+          >
+            {props.title}
+          </Box>
+          <Box
+            float='right'
+          >
+            {props.category.name}
+          </Box>
         </Box>
         <Box
           noOfLines={3}
