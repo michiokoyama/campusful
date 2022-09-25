bc3618a5ad56f67d7e7266ec272c99e54b9e8e6c
koyama <kmiccho@gmail.com>
2022-09-25 18:06:08 +0900

    Refactor: トップページのComponentを細分化する

--- web/frontend/src/pages/top/routes/Top.tsx
+++ web/frontend/src/pages/top/routes/Top.tsx
@@ -6,6 +6,7 @@ import {
   Center,
   ChakraStyledOptions,
   Flex,
+  Input,
   useDisclosure,
   useColorModeValue,
   color,
@@ -20,6 +21,7 @@ import { createTaggedTemplate } from 'typescript';
 import sanitizeHtml from 'sanitize-html'
 import { SidebarContent } from '../../../components/common/SideBar'
 import { colors } from '../../../const'
+import { ArticleType, GetArticlesQuery } from 'generated/graphql';
 
 export const Top = () => {
   return (
@@ -49,122 +51,108 @@ const MainContents = () => {
   </>)
 }
 
-const ArticleLists = (props: ChakraStyledOptions) => {
+const ArticleLists = (props: GetArticlesQuery['articles'][number]) => {
   const { isOpen, onToggle, onClose } = useDisclosure();
   const noOfLinesOfContent = isOpen ? undefined : 3
   const createdAt = new Date(props.createdAt)
   const displayCreatedAt = createdAt.getFullYear() + '/' + (createdAt.getMonth() + 1) + '/' + (createdAt.getDate())
-  const toggleOpen = () => {
 
+  // タイトル
+  const Title = (props: {title: string}) => {
+    return (<Box
+      fontWeight='bold'
+      fontSize={'xl'}
+      lineHeight='tight'
+      mt='1'
+      noOfLines={2}
+      onClick={onToggle}
+      _hover={{cursor: 'pointer'}}
+    >
+      {props.title}
+    </Box>
+    )
   }
+
+  // 本文
+  const Content = (props: {content: string}) => {
+    return (
+      <Box
+        dangerouslySetInnerHTML={{__html: sanitizeHtml(props.content)}}
+        noOfLines={noOfLinesOfContent}
+        onClick={onToggle}
+        _hover={{cursor: 'pointer'}}
+      >
+      </Box>
+    )
+  }
+
+  const Category = (props: {category: {name?: string}, type: ArticleType, universityName: string }) => {
+    return (
+      <Box display='flex' float={'left'}>
+        {props.category?.name ?
+        <Badge variant='solid' px='2' bg={colors.theme} fontSize='xs'>
+          {props.category.name}
+        </Badge>
+          : <></>
+        }
+        <Badge variant='outline' px='2' borderColor={'gray'}>
+          {props.type}
+        </Badge>
+        <Box as='span' color='gray.600' fontSize='xs' pl={'7px'}>
+          投稿日: {displayCreatedAt}
+        </Box>
+       <Box
+          color='gray.500'
+          fontWeight='semibold'
+          letterSpacing='wide'
+          fontSize='xs'
+          textTransform='uppercase'
+          pl={'7px'}
+        >
+          {props.universityName} 
+        </Box>
+      </Box>
+    )
+  }
+
+  const Comment = (props: {thanksNum: number, commentNum: number}) => {
+    return (
+      <Box display='flex' float={'right'}>
+        <Box as='span' ml='2' color='gray.600' fontSize='sm'>
+          <Box as='span' float={'left'} paddingTop={0.5}>
+            <HiAcademicCap size={'20px'}/>
+          </Box>
+          <Box as='span' paddingLeft='1' paddingRight={3}>
+            {props.thanksNum}
+          </Box>
+        </Box>
+        <Box as='span' ml='2' color='gray.600' fontSize='sm'>
+          <ChatIcon />
+          <Box as='span' paddingLeft='1' paddingRight={3}>
+            {props.commentNum}
+          </Box>
+        </Box>
+      </Box>
+    )
+  }
+
   return (
     <Box maxW='xg' borderWidth='1px' borderRadius='lg' overflow='hidden' marginBottom={'10px'}>
       <Box pt='3' pb='6' pl='6' pr='6' bg={'white'}>
-        <Box
-          fontWeight='bold'
-          fontSize={'xl'}
-          lineHeight='tight'
-          mt='1'
-          noOfLines={2}
-          onClick={onToggle}
-          _hover={{cursor: 'pointer'}}
-        >
-          {props.title}
-        </Box>
-
-        <Box
-          dangerouslySetInnerHTML={{__html: sanitizeHtml(props.content)}}
-          noOfLines={noOfLinesOfContent}
-          onClick={onToggle}
-          _hover={{cursor: 'pointer'}}
-        >
-          </Box>
-
-        <Box mt='2' mb='5'>
-          <Box display='flex' float={'left'}>
-            {props.category?.name ?
-            <Badge variant='solid' px='2' bg={colors.theme} fontSize='xs'>
-              {props.category.name}
-            </Badge>
-              : <></>
-            }
-            <Badge variant='outline' px='2' borderColor={'gray'}>
-              {props.type}
-            </Badge>
-            <Box as='span' color='gray.600' fontSize='xs' pl={'7px'}>
-              投稿日: {displayCreatedAt}
-            </Box>
-           <Box
-              color='gray.500'
-              fontWeight='semibold'
-              letterSpacing='wide'
-              fontSize='xs'
-              textTransform='uppercase'
-              pl={'7px'}
-            >
-              {props.author.university.name} 
-            </Box>
-          </Box>
-
-          <Box display='flex' float={'right'}>
-            <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-              <Box as='span' float={'left'} paddingTop={0.5}>
-                <HiAcademicCap size={'20px'}/>
-              </Box>
-              <Box as='span' paddingLeft='1' paddingRight={3}>
-                {props.thanksNum}
-              </Box>
-            </Box>
-            <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-              <ChatIcon />
-              <Box as='span' paddingLeft='1' paddingRight={3}>
-                {props.commentNum}
-              </Box>
-            </Box>
-          </Box>
-        </Box>
-      </Box>
-    </Box>
-  )
-
-  return (
-    <Box maxW='xg' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-      <Box p='6'>
-        <Box display='flex' alignItems='baseline'>
-          <Box
-            color='gray.500'
-            fontWeight='semibold'
-            letterSpacing='wide'
-            fontSize='xs'
-            textTransform='uppercase'
-            ml='2'
-          >
-            {/* {props.universityName} &bull; {props.userName}  */}
-            {props.name}
-          </Box>
-        </Box>
+        <Title title={props.title} />
+        <Content content={props.content} />
+      <Box mt='2' mb='5'>
+         <Category
+            category={props.category}
+            type={props.type}
+            universityName={props.author.university.name}
+          />
+          <Comment thanksNum={props.thanksNum} commentNum={props.commentNum} />
+        </Box> {/* カテゴリ、投稿日 */}
         <Box>
-          <Box
-            mt='1'
-            fontWeight='semibold'
-            as='h4'
-            lineHeight='tight'
-            noOfLines={1}
-          >
-            {props.title}
-          </Box>
-          <Box
-            float='right'
-          >
-            {props.category.name}
-          </Box>
+          <Input placeholder='コメントを追加する' />
         </Box>
-        <Box
-          noOfLines={3}
-        >
-            {props.content}
-        </Box>
-      </Box>
+      </Box> {/* 記事全体 padding */}
     </Box>
   )
 }
